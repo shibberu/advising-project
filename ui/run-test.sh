5c55eb5f00a680eac3315db32642c4eb09309c37
Shiroge Kiyohara <jiaoq@rose-hulman.edu>
2024-05-20 12:40:18 -0400

    update

--- ui/run-test.sh
+++ ui/run-test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 source .env
 source venv/bin/activate
-
 if [ "$(lsof -t -i:5001)" ]; then
     echo "Port 5001 is in use. Killing process..."
     kill -9 $(lsof -t -i:5001)
 fi
-
-# Run in the background
+Run in the background
 nohup python ui.py test > log-test.txt 2>&1 &
--- ui/ui.py
+++ ui/ui.py
@@ -10,37 +10,60 @@ from bedrock import stream_response
 
 app = Flask(__name__)
 
-from RAG_jiao.jiao_rag import RAG
-from RAG_jiao.query_processing import expand_course_code_in_query 
+from RAG_jiao.rag_main import RAG
+from RAG_jiao.query_processing.query_processing import expand_course_code_in_query 
+from RAG_jiao.query_processing.query_processing import expand_subject_abbreviation
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 import pickle
-with open('../RAG_jiao/util/course_code_to_name_dict.pkl', 'rb') as f:
+with open('../RAG_jiao/query_processing/course_code_to_name_dict.pkl', 'rb') as f:
     course_dict = pickle.load(f)
 
+CUDA_DEVICE = 'cuda:6'
 model_name = "BAAI/bge-base-en-v1.5"
-model_kwargs = {"device": 'cuda:1'}
+model_kwargs = {"device": CUDA_DEVICE}
 encode_kwargs = {"normalize_embeddings": True}
 bge_emb = HuggingFaceBgeEmbeddings(
     model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
 )
 
-my_rag = RAG(cuda_device='cuda:6')
-my_rag.load_data_from_file('../RAG_jiao/data/new_complete_dataset_credit_replaced.txt', 'full_data')
-my_rag.load_data_from_file('../RAG_jiao/data/new_complete_dataset_credit_replaced.txt', 'full_data_lemma', content_key='lemma')
+my_rag = RAG(cuda_device=CUDA_DEVICE)
+my_rag.load_data_from_file('../RAG_jiao/data/Sem75-Complete.txt', 'full_data')
+my_rag.load_data_from_file('../RAG_jiao/data/Sem75-Complete.txt', 'full_data_lemma', content_key='lemma')
 print('Dataset loaded')
 
 my_rag.create_dense_vector_index(bge_emb, 'bge_embedding', 'full_data')
-my_rag.create_bm25_index('bm25_retriever', 'full_data_lemma')
+my_rag.create_bm25_index('bm25_retriever', 'full_data_lemma', top_k=3)
 print('Indexes created')
 
 def get_similar_docs(q):
     q = expand_course_code_in_query(q, course_dict)
-    res = my_rag.dense_retrieval(q, 'bge_embedding', use_mmr=True)
+    q = expand_subject_abbreviation(q)
+
+    # retrieve 3 with each retriever
+    res = my_rag.dense_retrieval(q, 'bge_embedding', use_mmr=True, top_k=3)
+
+    # retrieve 3 with each retriever
     res1 = my_rag.bm25_retrieval(q, 'bm25_retriever', if_lemmatize=True)
+
+    # rerank
     hybrid_res = res + res1
+    hybrid_res = my_rag.remove_duplicate_doc(hybrid_res)
     rerank_res = my_rag.rerank(q, hybrid_res)
-    return [doc[0].page_content for doc in rerank_res]
+
+    # take the top 4 chunks
+    rerank_res = rerank_res[:4]
+    res_more_context = []
+    ## For each semantically chunked chunk, we will also add the chunk following it to the context 
+    ## semantically chunked chunks are indices 0-1648
+    for chunk in rerank_res:
+        if (chunk.metadata['seq_num'] < 1649) :
+            res_more_context.append(chunk)
+            res_more_context.append(my_rag.get_document('full_data', chunk.metadata['seq_num'] + 1))
+        else :
+            res_more_context.append(chunk)
+    res_more_context = my_rag.remove_duplicate_doc(res_more_context)
+    return [doc[0].page_content for doc in res_more_context]
 
 @app.route('/')
 def index():
@@ -58,7 +81,7 @@ def generate_response():
 ---------------------
 {context}
 ---------------------
-Given the context information and not prior knowledge, answer the query.
+Given the context information and not prior knowledge, answer the query. If the context does not contain enough information to answer to query, don't answer. 
 Query: {query}
 Answer:
 """
